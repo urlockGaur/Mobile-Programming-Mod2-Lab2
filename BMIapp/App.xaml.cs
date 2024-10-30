ea9ae0a549a46b383a3f0acde85584792ccfe5a5
urlockGaur <126732165+urlockGaur@users.noreply.github.com>
2024-10-30 14:03:21 -0500

    initial commit

---
+++ BMIapp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BMIapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
